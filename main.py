f241b5a8c09737f4c3d1997ecb7f44b082c472b4
VictorBaas363 <v.c.a.baas@student.tue.nl>
2024-11-27 10:05:39 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-# main.py>
-#%%`
+import numpy as np
 
+def R1 (r1):
+    return r1
 
-def Simulation():
-    print("hello world")
+def R2 (r2):
+    return r2
 
+def K1 (r1,r2,threshhold=1):
+    r_total = r1 + r2
+    if (r_total) > threshhold:
+        k1 = r_total
+        return k1
+    else:
+        return 0
+    
+def K2 (k1, threshhold=0.2):
+    if k1 > threshhold:
+        k2 = k1 + threshhold
+        return k2
+    else:
+        return 0
+    
+def P1 (k1, threshhold = 0.1):
+    if k1 > threshhold:
+        p1 = 1/(1+np.exp(k1-threshhold))
+        return p1
+    else:
+        return 0
+    
+def K3 (p1, threshhold=0.01):
+    if p1 > threshhold:
+        k3 = p1 - threshhold
+        return k3
+    else:
+        return 0
+    
+def C (k2, k3, threshhold=0.15):
+    if min(k2,k3) > threshhold:
+        c = min(k2,k3)
+        return C
+    else:
+        return 0
+    
+def A (c, threshhold = 0.1):
+    if c > threshhold:
+        return True
 
-#%%
