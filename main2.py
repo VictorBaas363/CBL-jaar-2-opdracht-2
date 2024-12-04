11724a444a21a67bb6dd6392d990839f5deaf5b2
JesperRooverss <j.i.roovers@student.tue.nl>
2024-12-04 09:49:18 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 class Pathway:
+    '''A parent class that defines the threshhold, activation and apotosis'''
     def __init__(self, thr):
         self.thr = thr
 
     def activation(self, x, y):
+        '''x is the input of a element in the pathway, y is the output'''
         if x > self.thr:
             return y
         else:
@@ -16,8 +18,10 @@ class Pathway:
             return True
         else:
             return False
+        
 
-class Kinase(Pathway):       
+class Kinase(Pathway):  
+    '''A class containing the kinases'''     
     def kinase_1(self, x1, x2):
         x = x1 + x2
         y = x
@@ -27,18 +31,20 @@ class Kinase(Pathway):
         y = x + self.thr
         return self.activation(x, y)
 
-
     def kinase_3(self, x,):
         y = x - self.thr
         return self.activation(x, y)
+    
 
 class Phosphatase(Pathway):
+    '''A class for the phosphatase'''
     def phosphatase(self, x):
         y = 1/(1 + np.exp(x - self.thr))
         return self.activation(x, y)
 
 
 class Complex(Pathway):
+    '''A class for'''
     def complex(self, x1, x2):
         x = min(x1, x2)
         y = x
@@ -46,6 +52,7 @@ class Complex(Pathway):
 
 
 def simulation(r1, r2):
+    '''A function used to simulate the pathway, first makes the objects and then use functions on the objects. Returns apotosis (True or False)'''
     kin_1 = Kinase(1.0)
     kin_2 = Kinase(0.2)
     kin_3 = Kinase(0.01)
@@ -55,11 +62,12 @@ def simulation(r1, r2):
 
     kinase_1 = kin_1.kinase_1(r1, r2)
     kinase_2 = kin_2.kinase_2(kinase_1)
-    phosphatse = phos.phosphatase(kinase_1)
-    kinase_3 = kin_3.kinase_3(phosphatse)
+    phosphatase = phos.phosphatase(kinase_1)
+    kinase_3 = kin_3.kinase_3(phosphatase)
     complex = comp.complex(kinase_2, kinase_3)
     apoptosis = apop.Apoptosis(complex)
     print(apoptosis)
+    return apoptosis
 
 simulation(0.3, 0.8)
 
