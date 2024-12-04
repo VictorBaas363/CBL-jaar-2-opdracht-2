0a4c71526a62541874e177211e01347b1ff7e40b
JesperRooverss <j.i.roovers@student.tue.nl>
2024-12-04 09:22:15 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -4,48 +4,62 @@ class Pathway:
     def __init__(self, thr):
         self.thr = thr
 
-    def activation(self, y):
-        if y > self.thr:
+    def activation(self, x, y):
+        if x > self.thr:
             return y
         else:
             return 0
+        
+    def Apoptosis(self, x):
+        y = x
+        if y > self.thr:
+            return True
+        else:
+            return False
 
 class Kinase(Pathway):       
     def kinase_1(self, x1, x2):
-        y = x1 + x2
-        return self.activation(y)
+        x = x1 + x2
+        y = x
+        return self.activation(x, y)
 
     def kinase_2(self, x):
         y = x + self.thr
-        return self.activation(y)
+        return self.activation(x, y)
 
 
     def kinase_3(self, x,):
         y = x - self.thr
-        return self.activation(y)
+        return self.activation(x, y)
 
 class Phosphatase(Pathway):
     def phosphatase(self, x):
         y = 1/(1 + np.exp(x - self.thr))
-        return self.activation(y)
+        return self.activation(x, y)
 
 
 class Complex(Pathway):
     def complex(self, x1, x2):
-        y = min(x1, x2)
-        return self.activation(y)
+        x = min(x1, x2)
+        y = x
+        return self.activation(x, y)
 
-def Apoptosis(y, thr = 0.1):
-    if y > thr:
-        return True
-    else:
-        return False
 
 def simulation(r1, r2):
-    kinase_1 = Kinase(1.0).kinase_1(r1, r2)
-    kinase_2 = Kinase(0.2).kinase_2(kinase_1)
-    phosphatse = Phosphatase(0.1).phosphatase(kinase_1)
-    kinase_3 = Kinase(0.01).kinase_3(phosphatse)
-    complex = Complex(0.15).complex(kinase_2, kinase_3)
-    print(Apoptosis(complex))
+    kin_1 = Kinase(1.0)
+    kin_2 = Kinase(0.2)
+    kin_3 = Kinase(0.01)
+    phos = Phosphatase(0.1)
+    comp = Complex(0.15)
+    apop = Pathway(0.1)
+
+    kinase_1 = kin_1.kinase_1(r1, r2)
+    kinase_2 = kin_2.kinase_2(kinase_1)
+    phosphatse = phos.phosphatase(kinase_1)
+    kinase_3 = kin_3.kinase_3(phosphatse)
+    complex = comp.complex(kinase_2, kinase_3)
+    apoptosis = apop.Apoptosis(complex)
+    print(apoptosis)
+
+simulation(0.3, 0.8)
 
